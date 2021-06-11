ef399b677b5b57c9db5eee344ced4a1cb284bdf2
Kara <palumbokara@yahoo.com>
2021-06-10 19:54:54 -0600

    adding completed deck exercise

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Card from './Card'
 
@@ -7,27 +7,57 @@ let baseURL = "https://deckofcardsapi.com/api/deck";
 const Deck = () => {
   const [deck, setDeck] = useState([]);
   const [card, setCard] = useState([])
+  const [toggleTimer, setToggleTimer] = useState(false)
+  const timerId = useRef();
 
   useEffect(() => {
     async function getDeck() {
-      const res = await axios.get(`${baseURL}/new/shuffle/?deck_count=1`);
+      const res = await axios.get(`${baseURL}/new/shuffle/`);
       setDeck(res.data);
     }
     getDeck();
-  }, [])
+  }, [setDeck])
 
-  const getCard = async () => {
-    let deckId = deck.deck_id;
-    const res = await axios.get(`${baseURL}/${deckId}/draw/`);
-    setCard(res.data.cards[0])
-      console.log(res.data.cards[0])
+
+
+  useEffect(() => {
+    async function drawCard() {
+      let deckId = deck.deck_id;
+      const draw = await axios.get(`${baseURL}/${deckId}/draw/`);
+
+      console.log(draw.data.remaining)
+
+      if (draw.data.remaining === 0) {
+        stopTimer();
+        throw new Error('Error: no cards remaining!')
+      } else {
+        setCard(draw.data.cards[0])
+      }
+    }
+
+    if (toggleTimer) {
+      timerId.current = setInterval(() => {
+        drawCard();
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(timerId.current)
+    }
+  }, [toggleTimer])
+
+  const stopTimer = () => {
+    clearInterval(timerId.current)
   }
+  
+  const handleCardDraw = async () => {
+    setToggleTimer(!toggleTimer)
+  };
 
   return (
     <>
-        <button onClick={getCard}>Gimme a card!</button>
+        <button onClick={handleCardDraw}>{toggleTimer ? "Stop Drawing!" : "Start Drawing!"}</button> 
         <Card cardImg={card.image}/>
-        
     </>
   )
 };
